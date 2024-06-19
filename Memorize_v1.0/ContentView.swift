79541eb9cd6fb9e5627804d2affb6e5a66ff2e0c
cannduran <canahmetduran0@gmail.com>
2024-06-19 22:28:29 +0300

    Initial Commit

---
+++ Memorize_v1.0/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Memorize_v1.0
+//
+//  Created by Can Ahmet Duran on 19.06.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Memorize_v1.0/Memorize_v1_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize_v1_0App.swift
+//  Memorize_v1.0
+//
+//  Created by Can Ahmet Duran on 19.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct Memorize_v1_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
